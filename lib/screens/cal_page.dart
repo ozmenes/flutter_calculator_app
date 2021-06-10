494f334a7d8e242a5e719fa6aab6969c8e896cf6
EnesOZMEN <ozmen.enes@hotmail.com>
2021-06-11 00:29:40 +0200

    Initial commit

---
+++ lib/screens/cal_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CalculatorPage extends StatefulWidget {
+  const CalculatorPage({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.grey,
+    );
+  }
+}
---
+++ lib/widgets/cal_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculatorWidget extends StatefulWidget {
+  const CalculatorWidget({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorWidgetState createState() => _CalculatorWidgetState();
+}
+
+class _CalculatorWidgetState extends State<CalculatorWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
