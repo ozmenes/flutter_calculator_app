f38077a065443c1c478138e2201b8e5d8bc8f957
EnesOZMEN <ozmen.enes@hotmail.com>
2021-06-16 00:50:25 +0200

    Initial commit

--- lib/screens/cal_page.dart
+++ lib/screens/cal_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator_app/widgets/cal_widget.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:google_fonts/google_fonts.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class CalculatorPage extends StatefulWidget {
   const CalculatorPage({Key? key}) : super(key: key);
@@ -8,10 +12,243 @@ class CalculatorPage extends StatefulWidget {
 }
 
 class _CalculatorPageState extends State<CalculatorPage> {
+  late String _history = '';
+  late String _expression = '';
+
+  void allClear(String text){
+    setState(() {
+      _history ='';
+      _expression  = '';
+    });
+  }
+  void numClick(String text) {
+    setState(() => _expression += text);
+  }
+  void clear(String text) {
+    setState(() {
+      _expression = '';
+    });
+  }
+
+  void evaluate(String text) {
+    Parser p = Parser();
+    Expression exp = p.parse(_expression);
+    ContextModel cm = ContextModel();
+
+    setState(() {
+      _history = _expression;
+      _expression = exp.evaluate(EvaluationType.REAL, cm).toString();
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.grey,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            /// previous cal..
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text(
+                  _history,
+                  style: GoogleFonts.rubik(
+                    textStyle: const TextStyle(
+                      fontSize: 24,
+                      color: Color(0xFF545F61),
+                    ),
+                  ),
+                ),
+              ),
+              alignment: const Alignment(1.0, 1.0),
+            ),
+            /// _expression
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Text(
+                  _expression,
+                  style: GoogleFonts.rubik(
+                    textStyle: const TextStyle(
+                      fontSize: 48,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              alignment: const Alignment(1.0, 1.0),
+            ),
+            /// Divider
+            const Divider(
+              endIndent: 15.0,
+              indent: 15.0,
+              thickness: 2,
+              height: 5,
+              color: Colors.deepOrange,
+            ),
+            const SizedBox(height: 20),
+            /// Ac C  % /
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalculatorButton(
+                  text: 'AC',
+                  fillColor: 0xFF6C807F,
+                  textSize: 20,
+                  callback: allClear,
+                ),
+                CalculatorButton(
+                  text: 'C',
+                  fillColor: 0xFF6C807F,
+                  textSize: 20,
+                  callback: clear,
+                ),
+                CalculatorButton(
+                  text: '%',
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                  textSize: 24,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '/',
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                  textSize: 24,
+                  callback: numClick,
+                ),
+              ],
+            ),
+            /// 7 8 9 X
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalculatorButton(
+                  text: '7',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '8',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '9',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: 'X',
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                  textSize: 24,
+                  callback: numClick,
+                ),
+              ],
+            ),
+            /// 4 5 6 -
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalculatorButton(
+                  text: '4',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '5',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '6',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '-',
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                  textSize: 38,
+                  callback: numClick,
+                ),
+              ],
+            ),
+            /// 1 2 3 +
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalculatorButton(
+                  text: '1',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '2',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '3',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '+',
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                  textSize: 30,
+                  callback: numClick,
+                ),
+              ],
+            ),
+            /// . 0 00 =
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CalculatorButton(
+                  text: '.',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '0',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '00',
+                  fillColor: 0xFFFF5722,
+                  textSize: 20,
+                  callback: numClick,
+                ),
+                CalculatorButton(
+                  text: '=',
+                  fillColor: 0xFFFFFFFF,
+                  textColor: 0xFF65BDAC,
+                  textSize: 30,
+                  callback: evaluate,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/cal_widget.dart
+++ lib/widgets/cal_widget.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:google_fonts/google_fonts.dart';
 
-class CalculatorWidget extends StatefulWidget {
-  const CalculatorWidget({Key? key}) : super(key: key);
+class CalculatorButton extends StatelessWidget {
+  final String text;
+  final int fillColor;
+  final int textColor;
+  final double textSize;
+  final Function callback;
 
-  @override
-  _CalculatorWidgetState createState() => _CalculatorWidgetState();
-}
+  const CalculatorButton({
+    Key? key,
+    required this.text,
+    required this.fillColor,
+    this.textColor = 0xFFFFFFFF,
+    this.textSize = 28,
+    required this.callback,
+  }) : super(key: key);
 
-class _CalculatorWidgetState extends State<CalculatorWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: SizedBox(
+        width: 65,
+        height: 65,
+        // ignore: deprecated_member_use
+        child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+          onPressed: () {
+            callback(text);
+          },
+          child: Text(
+            text,
+            style: GoogleFonts.rubik(
+              textStyle: TextStyle(
+                fontSize: textSize,
+              ),
+            ),
+          ),
+          // ignore: unnecessary_null_comparison
+          color: fillColor != null ? Color(fillColor) : null,
+          textColor: Color(textColor),
+        ),
+      ),
+    );
   }
 }
